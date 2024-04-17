caed8d0bf851d0b1806896cbd318d8eedb2980a3
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2024-04-16 22:22:28 -0500

    server side rendering con provide http client with fetch segun recomendacion de angular cli

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideRouter(routes,
       withViewTransitions(
         {
