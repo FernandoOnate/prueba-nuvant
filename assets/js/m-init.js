9aafdd1b221643493893f5a3536ead19b2ad8959
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2024-04-17 03:11:27 -0500

    Auto-generated commit

--- assets/js/m-init.js
+++ assets/js/m-init.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
+
+  // var elems = document.querySelector('#modal1');
+  // var instances = M.Modal.init(elems, {});
 });
+
